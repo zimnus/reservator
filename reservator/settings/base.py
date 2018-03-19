d373641c3d131688c9e8f7cf0c8e1d441a28c590
zimnus <mars.xaker@gmail.com>
2018-03-19 14:43:01 +0200

    Debug true

--- reservator/settings/base.py
+++ reservator/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '%e(sv$t)!#anz=0c5*sy@l^8-!i10s)9a=^oc09owysyd6xla5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', ]
 
